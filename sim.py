6e4665a583c7d8113ef0d5a07da8495af3cb8242
holistudio <daniel.bin.lu@gmail.com>
2025-03-07 23:29:42 -0500

    Control when simulation loop terminates

--- sim.py
+++ sim.py
@@ -3,9 +3,28 @@ import time
 TIMESTEP = 1
 
 def main():
-    print("Hello, world!")
-    time.sleep(TIMESTEP)
-    print("Hello again!")
+    # intialize sim time
+    t = 0
+
+    # intialize terminal state
+    terminal = False
+
+    print('### START SIMULATION ###')
+    # simulation loop
+    while not terminal:
+        print(f'TIMESTEP = {t}')
+
+        # increment sim time
+        t += 1
+
+        # wait TIMESTEP
+        time.sleep(TIMESTEP)
+
+        # check terminal state
+        if t > 10:
+            terminal = True
+            print('### END SIMULATION ###')
+        
 
 if __name__ == "__main__":
     main()
