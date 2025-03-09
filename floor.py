4cc7c0645d392a5c91c21568d1658a147ed34d81
holistudio <daniel.bin.lu@gmail.com>
2025-03-09 01:12:46 -0500

    Update floor.py
    Added set_floor() function.
    `floor_above` and `floor_below` are now internal attributes only accessed via getter/setters

--- floor.py
+++ floor.py
@@ -2,8 +2,8 @@ class Floor():
     def __init__(self, ID):
         super.__init__(object)
         self.ID = ID
-        self.floor_above = None
-        self.floor_below = None
+        self._floor_above = None
+        self._floor_below = None
         self.occupants = []
         self.button_state ={
             "up_button": False,
@@ -13,11 +13,22 @@ class Floor():
     
     def get_floor(self, relative='above'):
         if relative == 'above':
-            return self.floor_above
+            return self._floor_above
         elif relative == 'below':
-            return self.floor_below
+            return self._floor_below
         else:
             return 'Error: Invalid relative value. Only (above, below) are valid values.'
+    
+    def set_floor(self, floor, relative='above'):
+        if isinstance(floor, Floor) or floor is None:
+            if relative == 'above':
+                self._floor_above = floor
+            elif relative == 'below':
+                self._floor_below = floor
+            else:
+                return 'Error: Invalid relative value. Only (above, below) are valid values.'
+        else:
+            raise TypeError("next must be an instance of Floor or None")
 
 def test():
     print('FLOOR CLASS BASIC TEST')
