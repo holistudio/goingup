38a28ff8e97524e09c559e64d1b3c8e727fefe34
holistudio <daniel.bin.lu@gmail.com>
2025-03-09 00:59:12 -0500

    Floor class init()

--- floor.py
+++ floor.py
@@ -2,6 +2,13 @@ class Floor():
     def __init__(self, ID):
         super.__init__(object)
         self.ID = ID
+        self.floor_above = None
+        self.floor_below = None
+        self.occupants = []
+        self.button_state ={
+            "up_button": False,
+            "down_button": False
+        }
         pass
 
 def test():
