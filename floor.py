f172de56a50202a004c99c415cb8ee926aefa705
holistudio <daniel.bin.lu@gmail.com>
2025-03-09 00:56:39 -0500

    Create floor.py

---
+++ floor.py
@@ -0,0 +1,12 @@
+class Floor():
+    def __init__(self, ID):
+        super.__init__(object)
+        self.ID = ID
+        pass
+
+def test():
+    print('FLOOR CLASS BASIC TEST')
+    pass
+
+if __name__ == "__main__":
+    test()
