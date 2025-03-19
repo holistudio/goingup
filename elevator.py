94674f172dd5eb35bd10126fe632ce97cd471f26
holistudio <daniel.bin.lu@gmail.com>
2025-03-19 19:08:29 -0400

    Update elevator.py
    init() first pass

--- elevator.py
+++ elevator.py
@@ -1,6 +1,16 @@
 class Elevator(object):
     def __init__(self, ID):
         self.ID = ID
+        self._possible_status = ('stopped', 'moving')
+        self.status = self._possible_status[0]
+        self.floor_location = 0
+        self._possible_direction = ('none','up','down')
+        self.direction = self._possible_direction[0]
+        self.available_floors = []
+        self.button_states = []
+        self._possible_door_state = ('open', 'closed')
+        self.door_state = self._possible_door_state[0]
+        self.passengers = []
         pass
 
 def test():
