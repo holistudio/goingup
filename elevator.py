74d2998b1cec21c108876220aa28cd0da995f510
holistudio <daniel.bin.lu@gmail.com>
2025-03-19 18:53:36 -0400

    Create elevator.py

---
+++ elevator.py
@@ -0,0 +1,11 @@
+class Elevator(object):
+    def __init__(self, ID):
+        self.ID = ID
+        pass
+
+def test():
+    print('ELEVATOR CLASS TESTS')
+    pass
+
+if __name__ == "__main__":
+    test()
